2b3c30839ad9f73e69b134593d029580eda53e0b
juan.lastra <juan.lastra@ticketmaster.com>
2021-04-15 17:15:10 -0300

    Input enter function

--- TPE1/js/captcha.js
+++ TPE1/js/captcha.js
@@ -1,17 +1,27 @@
+document.getElementById("captchaButton").addEventListener("click", checkCaptcha);
+
+let captchaNumber;
+
+function setCaptchaNumber() {
+    captchaNumber = getRndInteger(1000, 9999);
+    document.getElementById("captcha").innerHTML = captchaNumber;
+};
+
+setCaptchaNumber();
+
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-let captchaNumber = getRndInteger(1000, 9999);
-
-document.getElementById("captcha").innerHTML = captchaNumber;
-
-document.getElementById("captchaButton").addEventListener("click", checkCaptcha);
-
 let captchaError = document.getElementById("captchaError");
 
-function checkCaptcha() {
+document.querySelector("#captchaField").addEventListener("keyup", event => {
+    if (event.key !== "Enter") return;
+    document.querySelector("#captchaButton").click();
+    event.preventDefault();
+});
 
+function checkCaptcha() {
 
     let captchaField = document.getElementById("captchaField");
     console.log(captchaField.value);
@@ -19,21 +29,15 @@ function checkCaptcha() {
     if (captchaField.value == captchaNumber) {
         alert("Enviado exitosamente");
     } else {
-
         captchaField.parentNode.insertBefore(captchaError, captchaField);
-
         captchaField.setAttribute("class", "error");
-
         captchaError.innerHTML = "Captcha Incorrecto";
-
     }
-
     setTimeout(function() {
         captchaField.setAttribute("class", "");
         captchaError.remove()
+        setCaptchaNumber();
 
     }, 3000);
-
-
 
 }
