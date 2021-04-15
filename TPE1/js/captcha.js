708e23ceda4c2c25dddc287c8945436662476ee0
juan.lastra <juan.lastra@ticketmaster.com>
2021-04-15 14:11:00 -0300

    TPE1 Test captcha

--- TPE1/js/captcha.js
+++ TPE1/js/captcha.js
@@ -1,8 +1,39 @@
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 let captchaNumber = getRndInteger(1000, 9999);
 
 document.getElementById("captcha").innerHTML = captchaNumber;
 
+document.getElementById("captchaButton").addEventListener("click", checkCaptcha);
+
+let captchaError = document.getElementById("captchaError");
+
+function checkCaptcha() {
+
+
+    let captchaField = document.getElementById("captchaField");
+    console.log(captchaField.value);
+
+    if (captchaField.value == captchaNumber) {
+        alert("Enviado exitosamente");
+    } else {
+
+        captchaField.parentNode.insertBefore(captchaError, captchaField);
+
+        captchaField.setAttribute("class", "error");
+
+        captchaError.innerHTML = "Captcha Incorrecto";
+
+    }
+
+    setTimeout(function() {
+        captchaField.setAttribute("class", "");
+        captchaError.remove()
+
+    }, 3000);
+
+
+
+}
