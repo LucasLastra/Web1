70dd2ea1cb94d5309c8c5b5e701c90f12268a440
juan.lastra <juan.lastra@ticketmaster.com>
2021-06-30 17:29:55 -0300

    cambios con rebase

--- TPE1/js/tablaBandas.js
+++ TPE1/js/tablaBandas.js
@@ -1,171 +1,70 @@
 "use strict"
-let url = "https://60d9dfbd5f7bf10017547810.mockapi.io/api/v1/bandas";
-
-async function getBandas() {
-    let htmlBandas = "";
-    try {
-        let respuesta = await fetch(url);
-        if (respuesta.status == 200) {
-            console.log("status OK");
-
-            let arrayBandas = await respuesta.json();
-
-            for (let i = 0; i < arrayBandas.length; i++) {
-                if (arrayBandas[i].banda.pokemon == true) {
-                    htmlBandas += `<tr>
-                    <td><mark>${arrayBandas[i].banda.genero}</mark></td>
-                    <td><mark>${arrayBandas[i].banda.nombre}</mark></td>
-                    <td><mark>${arrayBandas[i].banda.discografia}</mark></td>
-                   </tr>`;
-                } else {
-                    htmlBandas += `<tr>
-                    <td>${arrayBandas[i].banda.genero}</td>
-                    <td>${arrayBandas[i].banda.nombre}</td>
-                    <td>${arrayBandas[i].banda.discografia}</td>
-                   </tr>`;
-                }
-            }
-            document.querySelector("#tablaBandas").innerHTML = htmlBandas;
-        } else {
-            console.log("error de conección");
-let dataBandas = [];
+const url = "https://60d9dfbd5f7bf10017547810.mockapi.io/api/v1/bandas";
 
 //Defino acciones de los botones
 //Agregar un elemento
 document.getElementById("discosButtonAdd").addEventListener("click", loadData);
 
-document.getElementById("filterSearch").addEventListener("keyup", filterSearch);
-//Agrega 3 elementos sorpresa y random
-document.getElementById("discosButtonx3").addEventListener("click", randomElem);
 //Inserta los elementos a la tabla del arreglo de nuevos objetos para agregar
-document.getElementById("discosButtonSend").addEventListener("click", () => {
-    dataBandas = [...dataBandas, ...bandsToAdd];
-    fillTable();
-    bandsToAdd = [];
-    document.querySelector("#bandsToAdd").innerHTML = '';
-    document.querySelector("#bandsToAdd").classList.add('hide');
-});
-//Eliminar elementos de la tabla
-document.getElementById("discosButtonDelete").addEventListener("click", () => {
-    dataBandas = [];
-    fillTable();
-});
+document.getElementById("discosButtonSend").addEventListener("click", sendBandas);
 
-//Genero y meustro la tabla cuando se muestra la pagina
+// Filtro y search
+document.getElementById("filterSearch").addEventListener("keyup", filterSearch);
+
+//Genero y muestro la tabla cuando se muestra la pagina
 fillTable();
 
-function filterSearch() {
-    
-    var input, filter, table, tr, td, i, j, txtValue;
-    input = document.getElementById("filterSearch");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("tablaBandas");
-    tr = table.getElementsByTagName("tr");
-
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td");
-        for (j = 0; j < 3; j++) {
-            if (td[j]) {
-                txtValue = td[j].textContent || td[j].innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                    if (i < tr.length-1) {
-                        i++;
-                    }                    
-                } else {
-                    tr[i].style.display = "none";
-                }
-            }
-        }
-    }
-}
-
-function createButton(name, action) {
-
-    let button = document.createElement("button");
-    button.name = name;
-    button.classList.add("button");
-    button.innerHTML = action;
-    if (action === "edit") {
-        button.addEventListener('click', editRow);
-    }
-    if (action === "delete") {
-        button.addEventListener('click', deleteRow);
-    }
-    return button;
-}
-
-function editRow() {
-    let input = document.createElement("input");
-    let buttonSend = document.createElement("button");
-
-    this.disabled = true;
-
-    input.classList.add("input");
-    this.parentNode.previousElementSibling.replaceWith(input);
-
-    buttonSend.classList.add("button");
-    buttonSend.innerHTML = "SEND";
-    buttonSend.addEventListener('click', () => {
-        let body = {
-            "discografia": input.value
-        };
-        //doRequest(url, "PUT", body);
-        fillTable();
-    });
-
-    input.parentNode.insertBefore(buttonSend, input);
-
-}
-
-function deleteRow() {
-
-    //doRequest(url, "DELETE", this.name);
-
-}
-
 //Funcion para generar y mostrar la tabla
-function fillTable() {
+async function fillTable() {
     let htmlBandas = '';
     let tablaBandasDOM = document.querySelector("#tablaBandas");
     tablaBandasDOM.innerHTML = "";
 
-    //recorro el arreglo y sus objetos y creo las filas de la tabla
-    for (const banda of dataBandas) {
-        let row = document.createElement("tr");
-        for (const key in banda) {
-            let data = banda[key];
-            if (data.pokemon == true) {
-                htmlBandas += `<tr>
-                <td><mark>${data.genero}</mark></td>
-                <td><mark>${data.banda}</mark></td>
-                <td><mark>${data.discografia}</mark></td>
-               </tr>`;
-            } else {
-                htmlBandas = `
-                <td>${data.genero}</td>
-                <td>${data.banda}</td>
-                <td>${data.discografia}</td>
-               `;
+    try {
+        let respuesta = await doRequest(url, "GET");
+        if (respuesta.status == 200) {
+            console.log("status OK");
+
+            let dataBandas = await respuesta.json();
+            console.log(dataBandas);
+
+            //recorro el arreglo y sus objetos y creo las filas de la tabla
+            for (const banda of dataBandas) {
+                let row = document.createElement("tr");
+                for (const key in banda) {
+                    let data = banda[key];
+                    if (data.pokemon == true) {
+                        htmlBandas += `<tr>
+                        <td><mark>${data.genero}</mark></td>
+                        <td><mark>${data.nombre}</mark></td>
+                        <td><mark>${data.discografia}</mark></td>
+                        </tr>`;
+                    } else {
+                        htmlBandas = `
+                        <td>${data.genero}</td>
+                        <td>${data.nombre}</td>
+                        <td>${data.discografia}</td>
+                        `;
+                    }
+                    row.innerHTML = htmlBandas;
+                    let buttonEdit = document.createElement("td");
+                    buttonEdit.appendChild(createButton(banda.id, "edit"));
+                    let buttonDelete = document.createElement("td");
+                    buttonDelete.appendChild(createButton(banda.id, "delete"));
+                    row.appendChild(buttonEdit);
+                    row.appendChild(buttonDelete);
+                    tablaBandasDOM.appendChild(row);
+                }
             }
-            row.innerHTML = htmlBandas;
-            let buttonEdit = document.createElement("td");
-            buttonEdit.appendChild(createButton(data.banda, "edit"));
-            let buttonDelete = document.createElement("td");
-            buttonDelete.appendChild(createButton(data.banda, "delete"));
-            row.appendChild(buttonEdit);
-            row.appendChild(buttonDelete);
-            tablaBandasDOM.appendChild(row);
+        } else {
+            console.log("error de conexion");
         }
     } catch (error) {
         console.log(error);
     }
 }
-getBandas();
-
 
 let bandsToAdd = [];
-let pokemonsToAdd = [];
 //Funcion para agregar nuevos datos a la tabla
 function loadData() {
     let genre = document.getElementById("genre");
@@ -217,7 +116,6 @@ async function postBanda(bandsToAdd) {
         }
         doRequest(url, "POST", nuevaBanda);
     }
-    getBandas();
 }
 
 async function doRequest(url, method, body) {
@@ -230,76 +128,106 @@ async function doRequest(url, method, body) {
     }
     try {
         let res = await fetch(url, data)
-        console.log(res);
         return res;
     } catch (error) {
         console.log(error);
     }
 }
 
-async function post3Pokemons() {
-    await randomElem();
-    postBanda(pokemonsToAdd);
-    getBandas();
-}
-
-async function sendBandas(){
+async function sendBandas() {
     postBanda(bandsToAdd);
     bandsToAdd = [];
     document.querySelector("#bandsToAdd").innerHTML = 'Cargando...';
 
     //CAMBIAR!!!
     setTimeout(() => {
-        getBandas();
+        fillTable();
         document.querySelector("#bandsToAdd").innerHTML = '';
         document.querySelector("#bandsToAdd").classList.add('hide');
     }, 3000);
 }
 
-//Defino acciones de los botones
-//Agregar un elemento
-document.getElementById("discosButtonAdd").addEventListener("click", loadData);
-//Agrega 3 elementos sorpresa y random
-document.getElementById("discosButtonx3").addEventListener("click", post3Pokemons);
-//Inserta los elementos a la tabla del arreglo de nuevos objetos para agregar
-document.getElementById("discosButtonSend").addEventListener("click", sendBandas);
+function filterSearch() {
 
+    var input, filter, table, tr, td, i, j, txtValue;
+    input = document.getElementById("filterSearch");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tablaBandas");
+    tr = table.getElementsByTagName("tr");
 
-//Funcion para agregar 3 elementos random a la tabla
-async function randomElem() {
-    let pokemons = [];
-    for (let index = 0; index < 3; index++) {
-        //Elijo un numero al azar entre los primeros 150
-        let pokemon = getRndInteger(1, 150);
-        //Y traigo desde la pokeapi ese pokemon
-        pokemons.push(
-            await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
+    console.log(tr);
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < 3; j++) {
+            if (td[j]) {
+                txtValue = td[j].textContent || td[j].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    if (i < tr.length - 1) {
+                        i++;
+                    }
+                } else {
+                    tr[i].style.display = "none";
                 }
-            })
-        );
-    }
-
-    pokemonsToAdd = [];
-    //Con los datos de la pokeapi creo un nuevo objeto y lo agrego a la tabla 
-    for (const almostPokemon of pokemons) {
-        let pokemon = await almostPokemon.json();
-        let abilities = pokemon.abilities.map(e => e.ability.name);
-
-        let newObj = {};
-
-        newObj["banda"] = {
-            genero: pokemon.types[0].type.name,
-            nombre: pokemon.name,
-            discografia: abilities,
-            pokemon: true
+            }
         }
-        pokemonsToAdd.push(newObj);
     }
 }
-//Funcion para obtener un numero random
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+
+function createButton(name, action) {
+
+    let button = document.createElement("button");
+    button.name = name;
+    button.classList.add("button");
+    button.innerHTML = action;
+    if (action === "edit") {
+        button.addEventListener('click', editRow);
+    }
+    if (action === "delete") {
+        button.addEventListener('click', deleteRow);
+    }
+    return button;
 }
+
+async function editRow() {
+    let input = document.createElement("input");
+    let buttonSend = document.createElement("button");
+
+    this.disabled = true;
+    let id = this.name;
+
+    let response = await doRequest(url + `/${id}`, "GET");
+    let banda = await response.json();
+
+    input.value = banda['banda'].discografia[0];
+    input.classList.add("input");
+    this.parentNode.previousElementSibling.replaceWith(input);
+
+    buttonSend.classList.add("button");
+    buttonSend.innerHTML = "SEND";
+    buttonSend.addEventListener('click', () => {
+        
+        banda['banda'].discografia = [input.value];
+
+        console.log(banda);
+
+        doRequest(url + `/${id}`, "PUT", banda);
+        fillTable();
+    });
+
+    input.parentNode.insertBefore(buttonSend, input);
+
+}
+
+async function deleteRow() {
+
+    let id = this.name;
+
+    let response = await doRequest(url + `/${id}`, "DELETE");
+
+    if (response.ok) {
+        fillTable();
+    }
+}
+
